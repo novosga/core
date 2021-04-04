53ec72540569fe5bbd892597bf073d875307ca52
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2021-04-04 10:16:21 -0300

    Adding more properties to Customer

--- Entity/Cliente.php
+++ Entity/Cliente.php
@@ -11,6 +11,8 @@
 
 namespace Novosga\Entity;
 
+use DateTime;
+
 /**
  * Cliente.
  *
@@ -42,9 +44,30 @@ class Cliente implements \JsonSerializable
      * @var string
      */
     private $telefone;
+    
+    /**
+     * @var DateTime
+     */
+    private $dataNascimento;
+    
+    /**
+     * @var string
+     */
+    private $genero;
+    
+    /**
+     * @var Endereco
+     */
+    private $endereco;
+    
+    /**
+     * @var string
+     */
+    private $observacao;
 
     public function __construct()
     {
+        $this->endereco = new Endereco();
     }
 
     public function getId()
@@ -106,6 +129,54 @@ class Cliente implements \JsonSerializable
 
         return $this;
     }
+
+    public function getDataNascimento(): ?DateTime
+    {
+        return $this->dataNascimento;
+    }
+
+    public function setDataNascimento(?DateTime $dataNascimento): self
+    {
+        $this->dataNascimento = $dataNascimento;
+
+        return $this;
+    }
+
+    public function getGenero(): ?string
+    {
+        return $this->genero;
+    }
+
+    public function setGenero(?string $genero): self
+    {
+        $this->genero = $genero;
+
+        return $this;
+    }
+
+    public function getEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(Endereco $endereco): self
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+
+    public function getObservacao(): ?string
+    {
+        return $this->observacao;
+    }
+
+    public function setObservacao(?string $observacao): self
+    {
+        $this->observacao = $observacao;
+
+        return $this;
+    }
     
     public function __toString()
     {
@@ -120,6 +191,10 @@ class Cliente implements \JsonSerializable
             'documento' => $this->getDocumento(),
             'email'     => $this->getEmail(),
             'telefone'  => $this->getTelefone(),
+            'genero'  => $this->getGenero(),
+            'observacao'  => $this->getObservacao(),
+            'dataNascimento'  => $this->getDataNascimento() ? $this->getDataNascimento()->format('Y-m-d') : '',
+            'endereco'  => $this->getEndereco(),
         ];
     }
 }
---
+++ Entity/Endereco.php
@@ -0,0 +1,152 @@
+<?php
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novosga\Entity;
+
+/**
+ * Endereco
+ *
+ * @author Rogerio Lino <rogeriolino@gmail.com>
+ */
+class Endereco implements \JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $pais;
+    
+    /**
+     * @var string
+     */
+    private $cidade;
+
+    /**
+     * @var string
+     */
+    private $estado;
+
+    /**
+     * @var string
+     */
+    private $cep;
+
+    /**
+     * @var string
+     */
+    private $logradouro;
+
+    /**
+     * @var string
+     */
+    private $numero;
+
+    /**
+     * @var string
+     */
+    private $complemento;
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?string $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getComplemento(): ?string
+    {
+        return $this->complemento;
+    }
+
+    public function setComplemento(?string $complemento): self
+    {
+        $this->complemento = $complemento;
+
+        return $this;
+    }
+
+    public function getLogradouro(): ?string
+    {
+        return $this->logradouro;
+    }
+
+    public function setLogradouro(?string $logradouro): self
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    public function getCep(): ?string
+    {
+        return $this->cep;
+    }
+
+    public function setCep(string $cep): self
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    public function getEstado(): ?string
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(?string $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    public function getCidade(): ?string
+    {
+        return $this->cidade;
+    }
+
+    public function setCidade(?string $cidade): self
+    {
+        $this->cidade = $cidade;
+
+        return $this;
+    }
+
+    public function getPais(): ?string
+    {
+        return $this->pais;
+    }
+
+    public function setPais(?string $pais): self
+    {
+        $this->pais = $pais;
+
+        return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'pais' => $this->getPais(),
+            'estado' => $this->getEstado(),
+            'cidade' => $this->getCidade(),
+            'cep' => $this->getCep(),
+            'logradouro' => $this->getLogradouro(),
+            'numero' => $this->getNumero(),
+            'complemento' => $this->getComplemento(),
+        ];
+    }
+}
