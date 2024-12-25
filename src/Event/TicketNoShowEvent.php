9f03a91affa0a01368654748ae58f769c7f3806a
Rogerio Lino <rogeriolino@gmail.com>
2024-12-25 10:30:58 -0300

    feat: new ticket events

---
+++ src/Event/TicketNoShowEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novosga\Event;
+
+use Novosga\Entity\AtendimentoInterface;
+use Novosga\Entity\UsuarioInterface;
+
+/**
+ * TicketNoShowEvent
+ *
+ * @author Rogerio Lino <rogeriolino@gmail.com>
+ */
+final readonly class TicketNoShowEvent
+{
+    public function __construct(
+        public AtendimentoInterface $atendimento,
+        public UsuarioInterface $usuario,
+    ) {
+    }
+}
