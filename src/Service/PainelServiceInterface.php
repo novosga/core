ac8aa977afa069f39b7baeaab34609b119bb2844
Rogério Lino <rogeriolino@gmail.com>
2026-04-08 18:32:16 -0300

    feat: Painel entity (#14)

---
+++ src/Service/PainelServiceInterface.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novosga\Service;
+
+use Novosga\Entity\PainelInterface;
+use Novosga\Entity\UnidadeInterface;
+
+/**
+ * PainelServiceInterface
+ *
+ * @author Rogério Lino <rogeriolino@gmail.com>
+ */
+interface PainelServiceInterface
+{
+    public function getById(int $id): ?PainelInterface;
+
+    public function getByPublicId(string $publicId): ?PainelInterface;
+
+    /** @return PainelInterface[] */
+    public function findByUnidade(UnidadeInterface $unidade): array;
+
+    public function build(): PainelInterface;
+
+    public function save(PainelInterface $painel): PainelInterface;
+
+    public function remove(PainelInterface $painel): void;
+}
