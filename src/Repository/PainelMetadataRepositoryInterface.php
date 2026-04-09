ee6e832d2a101ca09f33cf1a4d639f6dc3d0074c
Rogerio Lino <rogeriolino@gmail.com>
2026-04-09 07:43:22 -0300

    feat: painel metadata

---
+++ src/Repository/PainelMetadataRepositoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Novo SGA project.
+ *
+ * (c) Rogerio Lino <rogeriolino@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Novosga\Repository;
+
+use Novosga\Entity\EntityMetadataInterface;
+use Novosga\Entity\PainelInterface;
+
+/**
+ * PainelMetadataRepositoryInterface
+ *
+ * @extends EntityMetadataRepositoryInterface<EntityMetadataInterface<PainelInterface>,PainelInterface>
+ *
+ * @author Rogerio Lino <rogeriolino@gmail.com>
+ */
+interface PainelMetadataRepositoryInterface extends EntityMetadataRepositoryInterface
+{
+}
